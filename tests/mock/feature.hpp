d7e9c2d18bd33e0217c4fda473d644101350c53a
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-08-08 18:08:18 -0400

    WIP feature tests; needs refactor to make platform impl non-static

--- tests/mock/feature.hpp
+++ tests/mock/feature.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <memory>
 #include <string>
 #include <string_view>
@@ -7,10 +8,11 @@
 
 #include "core/core.hpp"
 #include "core/feature.hpp"
+#include "core/storage.hpp"
 #include "platform/http_writer.hpp" // TODO: Move StringWriter out of platform
 
+#include "mock/filesystem.hpp"
 #include "mock/http_client.hpp"
-#include "mock/tempdir.hpp"
 
 using namespace datadog;
 
@@ -21,6 +23,11 @@ struct TLVBlockCopy
 {
     impl::TLVBlockType type;
     std::string data;
+
+    TLVBlockCopy(impl::TLVBlockType in_type, std::string_view in_data)
+        : type(in_type)
+        , data(in_data)
+    {}
 };
 
 /**
@@ -161,22 +168,189 @@ public:
     }
 };
 
-template<typename T = MockFeature>
-struct MockFeatureRegistration
-{
-    std::shared_ptr<T> impl;
-    std::unique_ptr<TempDirectory> tempdir;
-    std::unique_ptr<MockHttpClient> client;
-
-    std::vector<impl::RegisteredFeature>& vec_ref;
-    int32_t vec_i;
-
-    impl::RegisteredFeature& Registered()
-    {
-        return vec_ref[vec_i];
-    }
+static const CoreConfig MOCK_CORE_CONFIG{
+    TrackingConsent::Granted,
+    Site::us1,
+    "mock-client-token",
+    "mock-service",
+    "mock-env",
+    "mock-application-version",
+    BatchSize::Small,
+    UploadFrequency::Frequent,
+    BatchProcessingLevel::Low,
 };
 
-template<typename T = MockFeature>
-MockFeatureRegistration<T> RegisterMockFeature()
-{}
+/**
+ * Most of this state and logic is duplicated; we could feasibly use the actual Core
+ * implementation if we make subsystem init non-static and just do some creative
+ * static_casting.
+ */
+struct MockCore
+{
+    TrackingConsent tracking_consent{ MOCK_CORE_CONFIG.tracking_consent };
+    UploadFrequency upload_frequency{ MOCK_CORE_CONFIG.upload_frequency };
+    impl::CoreState state{ impl::CoreState::Initialized };
+    impl::CoreContext context{ MOCK_CORE_CONFIG };
+
+    // Subsystems created on init; in mock flavors
+    std::unique_ptr<MockStorageDirectory> storage_root;
+    std::unique_ptr<MockHttpClient> http_client;
+
+    // Registered features
+    std::vector<impl::RegisteredFeature> features;
+
+    // Storage and upload thread, tied to Start/Stop
+    std::unique_ptr<impl::Queue<impl::StorageMessage>> storage_queue;
+    std::optional<std::thread> storage_thread;
+    std::unique_ptr<impl::UploadScheduler> upload_scheduler;
+    std::optional<std::thread> upload_thread;
+
+    MockCore()
+    {
+        // Create mock versions of subsystems that the Core uses
+        storage_root = std::make_unique<MockStorageDirectory>();
+        http_client = std::make_unique<MockHttpClient>();
+    }
+
+    template<typename T = MockFeature>
+    void RegisterMockFeature(std::shared_ptr<T>& impl)
+    {
+        // Tests should only register Feature implementations
+        static_assert(std::is_base_of<impl::Feature, T>::value, "not a Feature");
+
+        // Tests should not call RegisterMockFeature() after calling Start()
+        if (state != impl::CoreState::Initialized)
+        {
+            assert(false && "RegisterMockFeature called after core start");
+            return;
+        }
+
+        // Create a subdirectory for this feature in the mock root storage dir
+        auto directory_result = storage_root->PrepareSubdirectory(impl->GetName());
+        assert(directory_result && "Failed to create mock feature storage directory");
+        std::unique_ptr<platform::IDirectory>& feature_subdir = *directory_result;
+
+        // Create directory interfaces for storage thread
+        const char* pending_name = impl::EventStorage::PENDING_SUBDIRECTORY_NAME;
+        auto pending_subdir = feature_subdir->PrepareSubdirectory(pending_name);
+        assert(pending_subdir && "Failed to create mock pending subdir");
+
+        const char* granted_name = impl::EventStorage::GRANTED_SUBDIRECTORY_NAME;
+        auto granted_subdir = feature_subdir->PrepareSubdirectory(granted_name);
+        assert(granted_subdir && "Failed to create mock granted subdir");
+
+        // Create BatchWriters and EventStorage interface for storage thread
+        auto event_storage = std::make_unique<impl::EventStorage>(
+            TrackingConsent::Granted,
+            std::make_unique<impl::BatchWriter>(std::move(*pending_subdir)),
+            std::make_unique<impl::BatchWriter>(std::move(*granted_subdir))
+        );
+
+        // Create second (reader) wrapper for granted dir, for upload thread
+        auto event_read_directory = feature_subdir->PrepareSubdirectory(granted_name);
+        assert(event_read_directory && "Failed to create mock event read directory");
+
+        // Create upload thread state for feature-specific timing etc.
+        // TODO: Don't pass UploadFrequency enum; pass actual timing parameters
+        auto upload_state = std::make_unique<impl::UploadThreadState>(upload_frequency);
+
+        // Register the feature
+        features.emplace_back(
+            impl->GetId(),
+            impl->GetName(),
+            impl,
+            std::move(feature_subdir),
+            std::move(event_storage),
+            std::move(*event_read_directory),
+            std::move(upload_state)
+        );
+    }
+
+    void Start()
+    {
+        // Tests should only call Start() while not running, after registering features
+        if (state != impl::CoreState::Initialized)
+        {
+            assert(false && "Start called while running");
+            return;
+        }
+        if (features.empty())
+        {
+            assert(false && "Start called with no features registered");
+            return;
+        }
+
+        // Spin up storage and upload thread
+        storage_queue = std::make_unique<impl::Queue<impl::StorageMessage>>();
+        storage_thread = std::thread(
+            impl::StorageThreadMain, std::ref(*storage_queue), std::ref(features)
+        );
+        upload_scheduler = std::make_unique<impl::UploadScheduler>();
+        upload_thread = std::thread(
+            impl::UploadThreadMain,
+            std::ref(context),
+            std::ref(*upload_scheduler),
+            std::ref(features),
+            std::ref(*http_client)
+        );
+
+        // Started!
+        state = impl::CoreState::Started;
+
+        // Route OnCoreStarted to all features, and install event-gen callback
+        for (const auto& feature : features)
+        {
+            const impl::FeatureId id = feature.id;
+            impl::EventGeneratedFunc event_callback =
+                [this, id](impl::Block event, impl::Block event_metadata) -> bool
+            {
+                return EnqueueStorageWrite(id, event, event_metadata);
+            };
+            feature.impl->OnCoreStarted(event_callback);
+        }
+    }
+
+    void Stop()
+    {
+        // Tests should only call Stop() while running
+        if (state != impl::CoreState::Started)
+        {
+            assert(false && "Stop called while not running");
+            return;
+        }
+
+        // Route OnCoreStopping to all features
+        for (const auto& feature : features)
+        {
+            feature.impl->OnCoreStopping();
+        }
+
+        // Shut down storage thread, then upload thread
+        storage_queue->Stop();
+        storage_thread->join();
+        storage_thread.reset();
+        storage_queue.reset();
+        upload_scheduler->Stop();
+        upload_thread->join();
+        upload_thread.reset();
+        upload_scheduler.reset();
+
+        // Stopped!
+        state = impl::CoreState::Initialized;
+    }
+
+    bool EnqueueStorageWrite(
+        impl::FeatureId feature_id,
+        impl::Block event,
+        impl::Block event_metadata
+    )
+    {
+        if (state != impl::CoreState::Started)
+        {
+            return false;
+        }
+        return storage_queue->Push(
+            impl::StorageMessage::EventGenerated(feature_id, event, event_metadata)
+        );
+    }
+};
--- tests/mock/feature_mock_test.cpp
+++ tests/mock/feature_mock_test.cpp
@@ -1,16 +1,37 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include "mock/filesystem.hpp"
-#include "mock/http_client.hpp"
-#include "mock/tempdir.hpp"
+#include "mock/feature.hpp"
 
 // These tests exercise the mock feature implementation to sanity check that it (and
 // the mock filesystem/http code it depends on) works as intended
 
+using namespace datadog;
+
+class CoolFeature : public MockFeature
+{
+public:
+    CoolFeature()
+        : MockFeature(impl::CreateFeatureId("COOL"), "coolstuff")
+    {}
+};
+
 TEST_CASE("MockFeature", "[unit]")
 {
-    SECTION("M yes")
+    SECTION("TEMP")
     {
+        // Given a registered feature in a running core
+        MockCore core;
+        auto feature = std::make_shared<CoolFeature>();
+        core.RegisterMockFeature(feature);
+        core.Start();
+
+        // When the feature generates an event
+        feature->GenerateEvent("hello world");
+        feature->GenerateEvent("goodbye", "metadata");
+
+        // And the Core is stopped
+        core.Stop();
+
         REQUIRE(2 == 2);
     }
 }
