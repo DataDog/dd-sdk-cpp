59a63c87872a1fc5343efe06a89eed3c43d1ada8
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-08-13 20:21:06 -0400

    feat(phase-1): Establish clearly-delineated C and C++ APIs
    refs: RUM-11310
    
    h## Rationale
    
    The existing `dd-sdk-cpp` codebase deals largely with C++: it does not
    yet have a C API.
    
    Additionally, while parts of the existing `core` module are nested in an
    `internal` namespace, with the `Core` itself using an inheritance
    hierarchy to separate public vs. private details, the architectural
    boundary between user-facing API surface and internal implementation
    details could be much clearer.
    
    RUM-11310 represents the effort to land on a design that:
    
    - Makes it abundantly clear which parts of the codebase are private and
      which parts are public
    
    - Prevents changes to private code from impacting ABI stability, as a
      default assumption
    
    - Retains an idiomatic, user-facing C++ API that allows customers to
      integrate the SDK into their C++ projects, and that would also
      facilitate building other C++-focused products on top of this SDK
    
    - Also exposes an idiomatic C API that would allow customers to
      integrate the SDK into their C projects, and that would also
      facilitate the creation of FFI bindings for other languages/runtimes
    
    As discussed in https://datadoghq.atlassian.net/wiki/spaces/RUMP/pages/5383880755/Conventions+for+C+SDK+development,
    I believe that a layered architecture with two independent APIs, one C
    and one C++, is the sanest approach for us to take.
    
    [image]
    
    h## Changes in this PR
    
    This PR includes:
    
    - Example programs, in both C and C++, demonstrating usage of both APIs
    - Public headers for the C API
    - Public headers for the C++ API
    - Source for the C API implementation
    - Source for the C++ API implementation
    - CMake configuration for examples and API bindings
    
    This PR does _not_ include any of the implementation-layer source
    referenced at the API binding layer. Those code changes will follow in
    later PRs.
    
    h### Caveats
    
    One notable omission: the super-cool copy-on-write attribute system is
    not yet represented in the logging API, as logging with arbitrary
    attributes is the last bit of SDK functionality left to work on
    in this refactor. (It now needs to be usable from both C and C++.)
    That'll be addressed in a later PR.
    
    Top-level SDK configuration is currently handled via structs that are
    exposed directly to the user: `dd_core_config_t` in C and `CoreConfig`
    in C++. Exposing the struct layouts publicly isn't ideal, as it makes it
    onerous for us to change these types, and it also doesn't let us
    validate user-supplied arguments. In future work, I intend to replace
    the C struct with an opaque type and option-setter functions (e.g.
    `dd_config_set_client_token(dd_config_t* config, const char* value)`),
    and I intend to replace the C++ struct with a `ConfigBuilder` type.

---
+++ include-cpp/datadog/logging.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+
+namespace datadog {
+
+// Forward declarations
+namespace impl {
+class Logger;
+class Logging;
+}
+
+enum class LogLevel
+{
+    Debug,
+    Info,
+    Notice,
+    Warn,
+    Error,
+    Critical,
+};
+
+struct LoggerConfig
+{
+    float remote_sample_rate = 1.0f;
+    std::optional<std::string> service;
+    std::optional<std::string> name;
+    LogLevel remote_log_threshold = LogLevel::Debug;
+};
+
+class Logger
+{
+public:
+    void Log(LogLevel level, std::string_view message);
+
+    void Debug(std::string_view message)
+    {
+        Log(LogLevel::Debug, message);
+    }
+
+    void Info(std::string_view message)
+    {
+        Log(LogLevel::Info, message);
+    }
+
+    void Notice(std::string_view message)
+    {
+        Log(LogLevel::Notice, message);
+    }
+
+    void Warn(std::string_view message)
+    {
+        Log(LogLevel::Warn, message);
+    }
+
+    void Error(std::string_view message)
+    {
+        Log(LogLevel::Error, message);
+    }
+
+    void Critical(std::string_view message)
+    {
+        Log(LogLevel::Critical, message);
+    }
+
+private:
+    std::unique_ptr<impl::Logger> _impl;
+};
+
+class Logging
+{
+public:
+    static std::shared_ptr<Logging> Register(class Core& core);
+
+private:
+    std::shared_ptr<impl::Logging> _impl;
+};
+
+}
---
+++ src/c/logging.cpp
@@ -0,0 +1,102 @@
+#include "datadog/logging.h"
+
+#include "datadog/core.h"
+#include "features/logging/logger.hpp"
+#include "features/logging/logging.hpp"
+#include "features/logging/types.hpp"
+
+#include "core_glue.hpp"
+
+#include <memory>
+
+struct dd_logger
+{
+    std::unique_ptr<datadog::impl::Logger> impl;
+};
+
+struct dd_logging
+{
+    std::shared_ptr<datadog::impl::Logging> impl;
+};
+
+// NOLINTBEGIN(cppcoreguidelines-owning-memory)
+
+extern "C" {
+
+dd_logging_t* dd_logging_init(dd_core_t* core)
+{
+    if (!core || !core->impl)
+    {
+        return nullptr;
+    }
+
+    auto impl = std::make_shared<datadog::impl::Logging>();
+    if (!core->impl->RegisterFeature(impl))
+    {
+        return nullptr;
+    }
+    dd_logging_t* logging = new dd_logging;
+    logging->impl = std::move(impl);
+    return logging;
+}
+
+void dd_logging_destroy(dd_logging_t* logging)
+{
+    delete logging;
+}
+
+dd_logger_t* dd_logger_create(dd_logging_t* logging, const dd_logger_config_t* config)
+{
+    if (!logging || !config)
+    {
+        return nullptr;
+    }
+
+    datadog::LoggerConfig cpp_config = datadog::LoggerConfig_FromC(*config);
+    dd_logger_t* logger = new dd_logger;
+    logger->impl = logging->impl->CreateLogger(cpp_config);
+    return logger;
+}
+
+void dd_logger_destroy(dd_logger_t* logger)
+{
+    delete logger;
+}
+
+void dd_logger_log(dd_logger_t* logger, dd_log_level_t level, const char* message)
+{
+    logger->impl->Log(datadog::LogLevel_FromC(level), message);
+}
+
+void dd_logger_debug(dd_logger_t* logger, const char* message)
+{
+    dd_logger_log(logger, DD_LOG_LEVEL_DEBUG, message);
+}
+
+void dd_logger_info(dd_logger_t* logger, const char* message)
+{
+    dd_logger_log(logger, DD_LOG_LEVEL_INFO, message);
+}
+
+void dd_logger_notice(dd_logger_t* logger, const char* message)
+{
+    dd_logger_log(logger, DD_LOG_LEVEL_NOTICE, message);
+}
+
+void dd_logger_warn(dd_logger_t* logger, const char* message)
+{
+    dd_logger_log(logger, DD_LOG_LEVEL_WARN, message);
+}
+
+void dd_logger_error(dd_logger_t* logger, const char* message)
+{
+    dd_logger_log(logger, DD_LOG_LEVEL_ERROR, message);
+}
+
+void dd_logger_critical(dd_logger_t* logger, const char* message)
+{
+    dd_logger_log(logger, DD_LOG_LEVEL_CRITICAL, message);
+}
+}
+
+// NOLINTEND(cppcoreguidelines-owning-memory)
