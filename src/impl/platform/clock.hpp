04125415abd91bb87c9376011c1a7006362d83e2
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-08-14 19:35:59 -0400

    feat(phase-4): Make clock a platform subsystem
    refs: RUM-11205
    
    This PR is a bit simpler than the last two: it adds our third and final
    (for now) platform subsystem: the clock.
    
    This interface, `IClock`, simply abstracts access to the system clock.
    This doesn't technically need to be a class, since it's only one
    dead-simple operation (reading the clock) that doesn't require any
    accompanying data. However, I chose to turn it into a full-fledged
    interface for consistency with the rest of the platform layer: it's
    constructed and injected into the core in the same way, with the same
    ownership semantics.
    
    Having a tiny bit of indirection on clock reads (rather than simply
    calling `std::chrono::system_clock::now()` directly) is handy for
    testing, but this also allows us to plug in a different clock
    implementation at build time.
    
    We're targeting C++17, so we _can_ assume `std::chrono` will be
    implemented on all target hardware, but the standard doesn't make any
    guarantees about the precision of `system_clock`, so presumably we're
    not taking it on faith that all platforms' implementations of
    `std::chrono` will suffice for our needs.
    
    Note that we previously cast values read from the clock to a raw
    `uint64_t`: it seems preferable to use `std::chrono` types so as to
    retain the type-safety they provide. I've chosen to store durations and
    timestamps from the platform clock interface as _signed_ `int64_t`, as
    it allows negative deltas, and the year 2262 is pretty far away.
    
    Code changes:
    
    - `clock.hpp` is the interface
    - `clock_std.cpp` is the std::chrono implementation
    - `clock_test.cpp` sanity-checks basic invariants
    - `mock/clock.hpp` is a mock implementation for use in tests

---
+++ src/impl/platform/clock.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <chrono>
+#include <cstdint>
+#include <memory>
+
+namespace datadog::platform {
+
+/**
+ * A duration compatible for comparison and arithmetic with an IClock timestamp, stored
+ * as a signed int64 count of nanoseconds.
+ */
+using Duration = std::chrono::nanoseconds;
+
+/**
+ * A point in time as measured by the system clock, expressed as a signed int64 count of
+ * nanoseconds since the Unix epoch.
+ */
+using Timestamp = std::chrono::time_point<std::chrono::system_clock, Duration>;
+
+/**
+ * Interface for the system clock.
+ *
+ * Timestamps sampled from IClock MUST have millisecond precision, at a minimum.
+ */
+class IClock
+{
+protected:
+    IClock() = default;
+
+public:
+    virtual ~IClock() = default;
+
+    // Copyable and movable: has no state
+    IClock(const IClock&) = default;
+    IClock& operator=(const IClock&) = default;
+    IClock(IClock&&) = default;
+    IClock& operator=(IClock&&) = default;
+
+    /**
+     * Returns the elapsed wall-clock time since the Unix epoch, i.e. since midnight
+     * January 1, 1970, UTC, in nanoseconds.
+     */
+    virtual Timestamp Now() const = 0;
+};
+
+struct Clock
+{
+    /**
+     * Creates an IClock interface used to read to the system clock.
+     */
+    static std::unique_ptr<IClock> Init();
+};
+
+} // namespace datadog::platform
---
+++ src/impl/platform/clock_std.cpp
@@ -0,0 +1,24 @@
+#include "platform/clock.hpp"
+
+#include <chrono>
+
+namespace datadog::platform {
+
+/**
+ * Implements IClock by sampling std::chrono::system_clock.
+ */
+class StdClock final : public IClock
+{
+public:
+    Timestamp Now() const final
+    {
+        return std::chrono::system_clock::now();
+    }
+};
+
+std::unique_ptr<IClock> Clock::Init()
+{
+    return std::make_unique<StdClock>();
+}
+
+} // namespace datadog::platform
