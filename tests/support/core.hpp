9ce6d2757881f46d596900a5498b6ee96534f0d7
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-08-19 15:41:03 -0400

    feat(phase-8): Refactor core implementation and feature interface
    refs: RUM-11313
    
    This PR refactors the main business logic of the core module, including
    the implementation of the Core, as well as the types that facilitate the
    implementation of modular Features that can be registered with the Core.
    
    In `feature.hpp`, we define the code required to implement a feature:
    
    - `FeatureId` is a unique FourCC code that tags a feature implementation
    - `BatchReader` is an interface that allows feature implementations to
      process event data (from a batch file that's open for read) in
      preparation for upload
    - `Report` is a description of an HTTP request prepared by a feature
      implementation from a batch of event data
    - `Feature` is the base class used for feature implementations,
      providing `WriteEvent` to allow the main thread to enqueue event data
      to be sent to the storage thread, and requiring an implementation for
      `UploadThread_PrepareReport` to prepare an HTTP request given a batch
      of such event data
    
    And in `core.hpp`, we define the core:
    
    - `CoreState` is used internally to manage the lifecycle of the Core as
      various initialization phases occur
    - `CoreSubsystems` contains all the platform-specific subsystems that
      are injected when the core is created
    - `RegisteredFeature` is the feature-specific state that's owned by the
      core
    - `Core` is the meat and potatoes of the SDK
    
    As with everything else, this code iterates on the existing
    implementation in `core.h` and `feature.h`, leaving the overall design
    and behavior largely unmodified. The key changes here mostly come down
    to:
    
    - Using the new platform subsystem interfaces, with clearly-defined
      dependency injection for easier mocking
    
    - Facilitating the new public API design, such that there's no longer a
      need to hide implementation details in an 'internal' inheritance layer
    
    - More explicit delineation of file-writer (storage) and file-reader
      (upload) semantics
    
    - More overt separation of storage and upload threads from the core
      implementation
    
    - Lock-free critical path for storage and upload of events
    
    - Support for feature improvements mentioned in previous PRs (e.g.
      delineation of consent-pending vs. consent-granted events, per-feature
      upload scheduling with adaptive backoff, zero-copy transfer of event
      data from batch file to HTTP connection, etc.)
    
    Note that the public interface of `impl::Core` is internal to the
    implementation layer - i.e. `Init()`, `SetService()`, and `SetEnv()` are
    not exposed publicly, and while it's possible to call `Stop()` on a
    running `impl::Core` and then restart it by calling `Start()` again,
    this expectation is not telegraphed via the public API.
    
    Finally, this PR includes some test code:
    
    - `mock/feature.hpp` provides a mock implementation of `Feature`
    - `support/core.hpp` provides `CoreTestHarness`, which bootstraps a
      working `impl::Core` with easily accessible mock subsystem
      implementations
    - `core_test.cpp` tests core lifecycle and feature registration
    - `feature_test.cpp` tests `BatchReader` and basic feature behavior re:
      producing and storing events

---
+++ tests/support/core.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "core/core.hpp"
+
+#include "mock/clock.hpp"
+#include "mock/filesystem.hpp"
+#include "mock/http_client.hpp"
+
+using namespace datadog;
+
+/**
+ * Default SDK configuration used in tests.
+ */
+static const CoreConfig MOCK_CORE_CONFIG{
+    TrackingConsent::Granted,
+    Site::us1,
+    "mock-client-token",
+    "mock-service",
+    "mock-env",
+    "mock-application-version",
+    BatchSize::Small,
+    UploadFrequency::Frequent,
+    BatchProcessingLevel::Low,
+};
+
+/**
+ * Encapsulates test setup, initializing a working Core implementation with mock
+ * implementations of platform subsystems.
+ *
+ * Allows you to register features (real or mock), start and stop the core, and examine
+ * the resulting filesystem state and network requests, while exercising the actual
+ * implementation of the Core and its storage and upload threads.
+ */
+struct CoreTestHarness
+{
+    impl::Core core;
+    MockClock& clock;
+    MockStorageDirectory& storage;
+    MockHttpClient& client;
+
+    explicit CoreTestHarness(
+        impl::Core&& in_core,
+        MockClock& in_clock,
+        MockStorageDirectory& in_storage,
+        MockHttpClient& in_client
+    )
+        : core(std::move(in_core))
+        , clock(in_clock)
+        , storage(in_storage)
+        , client(in_client)
+    {
+    }
+
+    static CoreTestHarness Init()
+    {
+        // Create mock implementations of required core subsystems
+        auto _clock = std::make_unique<MockClock>();
+        auto _storage_root = std::make_unique<MockStorageDirectory>();
+        auto _http = std::make_unique<MockHttpSubsystem>();
+
+        // Capture references to the underlying objects before we transfer ownership out
+        // of these unique_ptrs
+        MockClock& clock = *_clock;
+        MockStorageDirectory& storage = *_storage_root;
+        MockHttpSubsystem& http = *_http;
+
+        // Create the core, giving the core ownership of injected subsystems
+        impl::Core core(
+            MOCK_CORE_CONFIG,
+            impl::CoreSubsystems(
+                std::move(_clock), std::move(_storage_root), std::move(_http)
+            )
+        );
+
+        // Initialize the core: this should always succeed in tests
+        if (!core.Init())
+        {
+            assert(false && "core init failed in test setup");
+        }
+
+        // The core should have created an HTTP client on init; get a reference to it
+        assert(http.clients.size() == 1 && "core did not create 1 mock HTTP client");
+        MockHttpClient* client_ptr = http.clients[0];
+
+        // Return a struct that contains all the state we need in order to test - and
+        // examine the results of - code that interfaces with the core
+        return CoreTestHarness(std::move(core), clock, storage, *client_ptr);
+    }
+};
