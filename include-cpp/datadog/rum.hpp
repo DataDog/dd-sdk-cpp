dfeafdf33d038c22b53dc5d22e1d6640eea127a2
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-09-12 19:04:42 -0400

    RUM-11366, Step 3: Implement C++ API Header
    Created C++ API header for RUM feature following existing SDK patterns.
    Defines configuration struct and feature registration class using modern
    C++ idioms with RAII and shared ownership.
    
    - Create `include-cpp/datadog/rum.hpp`
    - Define `RumConfig` configuration struct
    - Define `Rum` class with static `Register(Core&)` method
    - Follow exact patterns from `logging.hpp` for consistency

---
+++ include-cpp/datadog/rum.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <memory>
+
+#include "datadog/api.hpp"
+
+namespace datadog {
+
+// Forward declarations
+namespace impl {
+class Rum;
+}  // namespace impl
+
+/**
+ * Configures the details of the RUM feature upon initialization.
+ */
+struct DATADOG_API RumConfig {
+  friend class Rum;
+  friend class impl::Rum;
+
+ public:
+  RumConfig() = default;
+};
+
+/**
+ * Interface to the Datadog SDK's RUM feature.
+ */
+class DATADOG_API Rum {
+ public:
+  /**
+   * Registers the RUM feature with the core of the Datadog SDK.
+   */
+  static std::shared_ptr<Rum> Register(
+      class Core& core, const RumConfig& config = RumConfig()
+  );
+
+ private:
+  std::shared_ptr<impl::Rum> _impl;
+};
+
+}  // namespace datadog
