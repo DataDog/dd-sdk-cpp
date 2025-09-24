07910a4d82283d07c75f902872aeb2ce917b5f07
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-09-24 12:00:22 +0200

    fix: Fix Win32 implementation

--- src/impl/uuid.cpp
+++ src/impl/uuid.cpp
@@ -10,6 +10,8 @@
 #include <uuid/uuid.h>
 #endif
 
+#include "assert.hpp"
+
 namespace datadog::impl {
 
 const uuid uuid::zero = {};
@@ -26,22 +28,23 @@ uuid uuid::make_random() {
 #ifdef _WIN32
   GUID guid;
   HRESULT hr = ::CoCreateGuid(&guid);
-  bytes[0] = static_cast<unsigned char>((guid.Data1 >> 24) & 0xFF);
-  bytes[1] = static_cast<unsigned char>((guid.Data1 >> 16) & 0xFF);
-  bytes[2] = static_cast<unsigned char>((guid.Data1 >> 8) & 0xFF);
-  bytes[3] = static_cast<unsigned char>((guid.Data1) & 0xFF);
-  bytes[4] = (unsigned char)((guid.Data2 >> 8) & 0xFF);
-  bytes[5] = (unsigned char)((guid.Data2) & 0xFF);
-  bytes[6] = (unsigned char)((guid.Data3 >> 8) & 0xFF);
-  bytes[7] = (unsigned char)((guid.Data3) & 0xFF);
-  bytes[8] = guid.Data4[0];
-  bytes[9] = guid.Data4[1];
-  bytes[10] = guid.Data4[2];
-  bytes[11] = guid.Data4[3];
-  bytes[12] = guid.Data4[4];
-  bytes[13] = guid.Data4[5];
-  bytes[14] = guid.Data4[6];
-  bytes[15] = guid.Data4[7];
+  DATADOG_ASSERT(SUCCEEDED(hr), "CoCreateGuid failed");
+  value.bytes[0] = static_cast<unsigned char>((guid.Data1 >> 24) & 0xFF);
+  value.bytes[1] = static_cast<unsigned char>((guid.Data1 >> 16) & 0xFF);
+  value.bytes[2] = static_cast<unsigned char>((guid.Data1 >> 8) & 0xFF);
+  value.bytes[3] = static_cast<unsigned char>((guid.Data1) & 0xFF);
+  value.bytes[4] = (unsigned char)((guid.Data2 >> 8) & 0xFF);
+  value.bytes[5] = (unsigned char)((guid.Data2) & 0xFF);
+  value.bytes[6] = (unsigned char)((guid.Data3 >> 8) & 0xFF);
+  value.bytes[7] = (unsigned char)((guid.Data3) & 0xFF);
+  value.bytes[8] = guid.Data4[0];
+  value.bytes[9] = guid.Data4[1];
+  value.bytes[10] = guid.Data4[2];
+  value.bytes[11] = guid.Data4[3];
+  value.bytes[12] = guid.Data4[4];
+  value.bytes[13] = guid.Data4[5];
+  value.bytes[14] = guid.Data4[6];
+  value.bytes[15] = guid.Data4[7];
 #else
   // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
   uuid_generate(value.bytes.data());
