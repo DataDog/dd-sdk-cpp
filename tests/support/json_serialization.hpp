1e03373560c0e578a9f6b36829df074276d9db30
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-10-29 15:11:17 -0400

    feat: Define JSON-schema-conformant RumViewEvent struct
    refs: RUM-12321
    
    This PR builds on recent JSON serialization changes to define
    `RumViewEvent`, based on [rum-events-format/schemas/rum/view-event.json](https://github.com/DataDog/rum-events-format/blob/master/schemas/rum/view-schema.json).
    
    Unlike the mobile SDKs, we don't have any custom tooling to generate C++
    types from the JSON schema. While such codegen tooling would certainly
    be helpful in the long run, here's what I've come up with for now:
    
    1. `RumViewEvent` and its constituent types are defined by hand, using a
       reasonable set of fairly rigid organizational conventions.
    
    2. Those conventions are strictly outlined in a prompt at the top of the
       file, such that you can just tell an LLM to "validate this file" and
       it will do a pretty good job of catching copy-paste errors and
       omissions.
    
    3. A set of unit tests validates that serializing a `RumViewEvent`
       yields a well-formed JSON object that is equivalent to an expected
       value, including a kitchen-sink test case that sets every known
       property.
    
    4. (Not in this PR:) A separate test utility (written in Python) grabs
       those expected JSON value payloads out of the .cpp test files and
       validates them against the `rum-events-format` schema, transitively
       proving that the values generated by passing unit tests are in fact
       valid RUM events.
    
    To make reviews easier, that validation tool will follow separately in
    the next PR.
    
    This PR does not update the RUM implementation to actually produce these
    events; that's the next task on my agenda (and the final change to close
    out RUM-12321).
    
    A few notes:
    
    - These types are meant to be treated as plain ol' data: they're just
      structs with public fields that mirror the JSON format 1:1
    
    - All required properties are defined such that they _must_ be
      initialized via an explicit constructor: if the compiler allows a
      `RumEventValue` to exist, then it ought to be in a state that will be
      serialized to a schema-conformant JSON object
    
    - On Clang/libc++, `sizeof(RumViewEvent)` is 1656 bytes. We're generally
      just initializing one value at a time in order to serialize it to JSON
      and push it onto the storage queue, so just reserving space for a
      temporary `RumViewEvent` on the stack every time we need to generate
      an event seems like it'd be perfectly performant.
    
    - Some fields use `std::string` and so _will_ perform a handful of small
      allocations depending on the number and length of the string values in
      use. If this becomes a performance concern we could define a "small
      string" type with inline storage for N bytes, so that we'd only
      heap-allocate for unexpectedly long values... but I don't think it's
      something we need to sweat at the moment.
    
    - Not all properties are present: I've omitted a handful of ancillary
      properties (e.g. highly web-specific or mobile-phone-specific stuff,
      video streaming, etc.) from this initial implementation, as well as
      some properties which are specified as JSON arrays (e.g. slow_frames,
      in_foreground_periods, page_states), which will need to be revisited
      later.

--- tests/support/json_serialization.hpp
+++ tests/support/json_serialization.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <cinttypes>
+#include <nlohmann/json.hpp>
 #include <string_view>
 #include <vector>
 
@@ -23,5 +24,23 @@ void RequireJsonLiteral(const T& value, std::string_view want) {
   std::vector<uint8_t> buffer;
   EncodeJson<T>(buffer, value);
   std::string_view got(reinterpret_cast<const char*>(buffer.data()), buffer.size());
+  REQUIRE(got == want);
+}
+
+/**
+ * Given a value of any JSON-serializable type T, serializes it with `EncodeJson`, then
+ * parses the encoded value with nlohmann:json to verify that it's a valid JSON object,
+ * then verifies that it is equivalent to the provided JSON object literal.
+ */
+template <typename T>
+void RequireJsonObject(const T& value, std::string_view want_text) {
+  auto want = nlohmann::json::parse(want_text);
+  REQUIRE(want.is_object());
+
+  std::vector<uint8_t> buffer;
+  EncodeJson<T>(buffer, value);
+  std::string_view text(reinterpret_cast<const char*>(buffer.data()), buffer.size());
+
+  auto got = nlohmann::json::parse(text);
   REQUIRE(got == want);
 }
