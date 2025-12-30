83c28080714f444946a59fb3314b9e5afd0622d2
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-12-30 15:52:30 -0600

    chore: Use correct URL for ci-identities-gitlab-job-client

--- ci/windows/install-ci-identities-client.ps1
+++ ci/windows/install-ci-identities-client.ps1
@@ -9,7 +9,7 @@ $ErrorActionPreference = 'Stop'
 # integration tests etc. - this requires that we use the ci-identities-gitlab-job-client
 # tool in order to assume the IAM role associated with our job's CI Identity
 # https://datadoghq.atlassian.net/wiki/spaces/SECENG/pages/5324145720/Using+CI+Identities
-$exeUrl = "https://www.python.org/ftp/python/$($Version)/python-$($Version)-amd64.exe"
+$exeUrl = "https://binaries-ddbuild-io-prod.s3.us-east-1.amazonaws.com/ci-identities/ci-identities-gitlab-job-client/versions/v$($Version)/ci-identities-gitlab-job-client-windows-amd64.exe"
 
 # Install to c:\devtools\ddbuild
 if(! (test-path "c:\devtools\ddbuild")){
