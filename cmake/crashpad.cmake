71830ebd7e0278e296ef18d6a0a82caba4966101
Alex Forsythe <alex.forsythe@datadoghq.com>
2026-01-13 14:56:45 -0500

    chore: Integrate crashpad build (via bootstrap script) into CMake build

---
+++ cmake/crashpad.cmake
@@ -0,0 +1,82 @@
+# Crashpad is part of the Chromium project, and it uses a Google-specific toolchain
+# (depot_tools, gclient, gn, ninja) rather than supporting CMake. We have a script in
+# tools/bootstrap-crashpad/ that streamlines the process of cloning and building
+# crashpad with its natively-supported tools, and we use the ExternalProject module to
+# integrate that build process into our CMake build.
+include(ExternalProject)
+
+# Our main library target for the C++ SDK (dd_native) will need to link against the
+# crashpad_client static lib once it's built. To ensure binary compatibility, we need to
+# resolve the relevant details of our CMake build configuration, so that we can produce
+# a GN build with the compatible set of options.
+get_target_property(DD_COMPILE_OPTIONS dd_native COMPILE_OPTIONS)
+get_target_property(DD_LINK_OPTIONS dd_native LINK_OPTIONS)
+
+# Convert lists to pipe-separated strings to avoid shell parsing issues
+# (CMake lists use semicolons, which would be expanded to separate arguments)
+# We use pipe '|' as delimiter since it won't appear in compiler flags
+if(DD_COMPILE_OPTIONS)
+    string(REPLACE ";" "|" DD_COMPILE_OPTIONS_ENCODED "${DD_COMPILE_OPTIONS}")
+else()
+    set(DD_COMPILE_OPTIONS_ENCODED "")
+endif()
+if(DD_LINK_OPTIONS)
+    string(REPLACE ";" "|" DD_LINK_OPTIONS_ENCODED "${DD_LINK_OPTIONS}")
+else()
+    set(DD_LINK_OPTIONS_ENCODED "")
+endif()
+
+# Declare an ExternalProject target, which we can build in order to bootstrap the
+# crashpad repo with all required tools and then build the static library. Note that
+# crashpad_external only encapsulates the commands required to download and build
+# crashpad; it doesn't tell CMake anything about that build's artifacts or dependencies
+# TODO(RUM-12207): Better support for incremental builds
+ExternalProject_Add(crashpad_external
+    #SOURCE_DIR ${CMAKE_SOURCE_DIR}/chromium/crashpad/crashpad
+    DOWNLOAD_COMMAND python3 ${CMAKE_SOURCE_DIR}/tools/bootstrap-crashpad/main.py install
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND python3 ${CMAKE_SOURCE_DIR}/tools/bootstrap-crashpad/main.py build
+        --no-install
+        -c CMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
+        -c CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -c CMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
+        -c DD_BUILD_SHARED=${DD_BUILD_SHARED}
+        -c DD_COMPILE_OPTIONS=${DD_COMPILE_OPTIONS_ENCODED}
+        -c DD_LINK_OPTIONS=${DD_LINK_OPTIONS_ENCODED}
+    INSTALL_COMMAND ""
+    USES_TERMINAL_DOWNLOAD TRUE
+    USES_TERMINAL_BUILD TRUE
+    BUILD_ALWAYS TRUE
+)
+
+# Resolve the expected path to the static lib file for the crashpad client library
+if(WIN32)
+    set(CRASHPAD_CLIENT_LIB_PATH ${CMAKE_SOURCE_DIR}/chromium/crashpad/crashpad/out/Default/obj/client/client.lib)
+else()
+    set(CRASHPAD_CLIENT_LIB_PATH ${CMAKE_SOURCE_DIR}/chromium/crashpad/crashpad/out/Default/obj/client/libclient.a)
+endif()
+
+# Establish a CMake target to represent the crashpad client static library: IMPORTED
+# creates a logical target that CMake expects to already be present at link-time rather
+# than building the library itself
+add_library(crashpad::client STATIC IMPORTED)
+set_target_properties(crashpad::client PROPERTIES
+    IMPORTED_LOCATION ${CRASHPAD_CLIENT_LIB_PATH}
+)
+
+# Establish that this static library file is produced by building crashpad_external
+add_custom_command(
+    OUTPUT ${CRASHPAD_CLIENT_LIB_PATH}
+    DEPENDS crashpad_external
+    COMMENT "Building crashpad via external project"
+)
+
+# Add a custom target to establish that depending on crashpad::client requires the
+# static library file to be built as a prerequisite
+add_custom_target(crashpad_client_file
+    DEPENDS ${CRASHPAD_CLIENT_LIB_PATH}
+)
+add_dependencies(crashpad::client crashpad_client_file)
+
+# Update the main library target for the SDK to link against the crashpad client
+target_link_libraries(dd_native PRIVATE crashpad::client)
--- tools/bootstrap-crashpad/main.py
+++ tools/bootstrap-crashpad/main.py
@@ -15,18 +15,15 @@ configuration. Those files are fed into GN in order to generate Ninja build file
 more information, see:
 
 https://chromium.googlesource.com/crashpad/crashpad/+/HEAD/doc/developing.md
-
-- TODO(RUM-12207): Integration into CMake build, enough to ensure that the crashpad
-  binaries produced are compatible with the build of dd-sdk-cpp as configured
-- TODO(RUM-12207): Versioning of gclient metadata to pin target version(s)
-- TODO(RUM-12207): Command-line control for different commands (upgrading to latest
-  upstream crashpad version vs. building vs. running tests)
 """
 import os
 import sys
+import json
+import platform
 import subprocess
 import argparse
-from typing import Dict, List
+from dataclasses import dataclass, field
+from typing import Dict, List, Set
 
 __repo_root__ = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 __chromium_root__ = os.path.join(__repo_root__, 'chromium')
@@ -41,6 +38,128 @@ __crashpad_test_binaries__ = [
     'crashpad_test_test',
     'crashpad_util_test',
 ]
+__crashpad_out_relpath__ = os.path.join('out', 'Default')
+__crashpad_out_abspath__ = os.path.join(__crashpad_repo_root__, __crashpad_out_relpath__)
+
+
+def _default_parallel_job_count() -> int:
+    # For CMake builds in CI, we pass '--parallel ${NUM_PARALLEL_BUILD_JOBS}' to ensure
+    # that builds don't overutilize resources, but this script is invoked from the
+    # BUILD_COMMAND of a CMake ExternalProject target, which does not have access to the
+    # CMAKE_BUILD_PARALLEL_LEVEL var, because the command is finalized at
+    # configure-time. Therefore, in CI we can't explicitly control --parallel and must
+    # instead check NUM_PARALLEL_BUILD_JOBS directly.
+    num_parallel_build_jobs = os.environ.get('NUM_PARALLEL_BUILD_JOBS', '')
+    if num_parallel_build_jobs:
+        try:
+            return int(num_parallel_build_jobs)
+        except ValueError:
+            pass
+
+    # Inferring from CPU count works fine for local builds, but in CI this reflects the
+    # CPU count of the host without respecting KUBERNETES_CPU_REQUEST etc.
+    return os.cpu_count() - 1
+
+
+def _gn_default_target_cpu():
+    machine = platform.machine().lower()
+    if machine in ("x86_64", "amd64"):
+        return "x64"
+    if machine in ("i386", "i686", "x86"):
+        return "x86"
+    if machine in ("aarch64", "arm64"):
+        return "arm64"
+    if machine.startswith("arm"):
+        return "arm"
+    raise RuntimeError(f"Unsupported architecture: {machine}")
+
+
+@dataclass
+class GnArgs:
+    is_debug: bool = False
+    target_cpu: str = _gn_default_target_cpu()
+    mac_deployment_target: str = ''
+    extra_cflags: Set[str] = field(default_factory=set)
+    extra_cflags_c: Set[str] = field(default_factory=set)
+    extra_cflags_cc: Set[str] = field(default_factory=set)
+    extra_cflags_objc: Set[str] = field(default_factory=set)
+    extra_cflags_objcc: Set[str] = field(default_factory=set)
+    extra_ldflags: Set[str] = field(default_factory=set)
+    extra_arflags: Set[str] = field(default_factory=set)
+
+    def __str__(self) -> str:
+        args = [
+            f'is_debug={json.dumps(self.is_debug)}',
+            f'target_cpu={json.dumps(self.target_cpu)}'
+        ]
+        if self.mac_deployment_target:
+            args += [f'mac_deployment_target={json.dumps(self.mac_deployment_target)}']
+        args += [
+            f'extra_cflags={json.dumps(" ".join(sorted(self.extra_cflags)))}',
+            f'extra_cflags_c={json.dumps(" ".join(sorted(self.extra_cflags_c)))}',
+            f'extra_cflags_cc={json.dumps(" ".join(sorted(self.extra_cflags_cc)))}',
+            f'extra_cflags_objc={json.dumps(" ".join(sorted(self.extra_cflags_objc)))}',
+            f'extra_cflags_objcc={json.dumps(" ".join(sorted(self.extra_cflags_objcc)))}',
+            f'extra_ldflags={json.dumps(" ".join(sorted(self.extra_ldflags)))}',
+            f'extra_arflags={json.dumps(" ".join(sorted(self.extra_arflags)))}',
+        ]
+        return ' '.join(args)
+
+    @classmethod
+    def from_cmake(cls, vars: Dict[str, str]) -> 'GnArgs':
+        args = GnArgs()
+
+        # crashpad requires C++20; validate CMAKE_CXX_STANDARD for compatibility
+        cmake_cxx_standard = vars.get('CMAKE_CXX_STANDARD', '')
+        if cmake_cxx_standard and cmake_cxx_standard != '20':
+            raise ValueError(f"Unsupported CMAKE_CXX_STANDARD: '{cmake_cxx_standard}'")
+
+        # is_debug: Initialize from CMAKE_BUILD_TYPE (required)
+        cmake_build_type = vars.get('CMAKE_BUILD_TYPE', '')
+        if cmake_build_type == 'Debug':
+            args.is_debug = True
+        elif cmake_build_type in ('Release', 'RelWithDebInfo', 'MinSizeRel'):
+            args.is_debug = False
+        else:
+            raise ValueError(f"Unsupported CMAKE_BUILD_TYPE: '{cmake_build_type}'")
+        
+        # target_cpu: Assume target arch is host arch (cross-compilation not supported)
+        args.target_cpu = _gn_default_target_cpu()
+
+        # mac_deployment_target: Initialize from CMAKE_OSX_DEPLOYMENT_TARGET
+        if sys.platform == 'darwin':
+            cmake_osx_deployment_target = vars.get('CMAKE_OSX_DEPLOYMENT_TARGET', '')
+            if not cmake_osx_deployment_target:
+                raise ValueError('CMAKE_OSX_DEPLOYMENT_TARGET must be specified for macOS builds')
+            args.mac_deployment_target = cmake_osx_deployment_target
+
+        # extra_cflags, extra_cflags_cc: Parse from DD_COMPILE_OPTIONS (pipe-separated);
+        # filter out warnings etc.
+        dd_compile_options_encoded = vars.get('DD_COMPILE_OPTIONS', '')
+        dd_compile_options = {x.strip() for x in dd_compile_options_encoded.split('|') if x.strip()}
+        for opt in dd_compile_options:
+            # TODO(RUM-12207): Filtering for MSVC args
+            if opt.startswith('-W'):
+                continue
+            args.extra_cflags.add(opt)
+            args.extra_cflags_cc.add(opt)
+
+        # extra_ldflags: Parse from DD_LINK_OPTIONS (pipe-separated)
+        dd_link_options_encoded = vars.get('DD_LINK_OPTIONS', '')
+        dd_link_options = {x.strip() for x in dd_link_options_encoded.split('|') if x.strip()}
+        for opt in dd_link_options:
+            args.extra_ldflags.add(opt)
+
+        # If building shared library, ensure PIC is enabled
+        dd_build_shared = vars.get('DD_BUILD_SHARED', 'OFF')
+        if dd_build_shared not in ('OFF', 'FALSE', '0', ''):
+            args.extra_cflags.add('-fPIC')
+            args.extra_cflags_cc.add('-fPIC')
+
+        # TODO(RUM-12207): MSVC /MT, /MD, /MTd, /MDd
+        # TODO(RUM-12207): Validate compiler/linker binary?
+
+        return args
 
 
 def _depot_tools_env() -> Dict[str, str]:
@@ -66,8 +185,7 @@ def clone_depot_tools():
     print(f'depot_tools present at: {__depot_tools_root__}')
 
 
-
-def fetch_crashpad(update=True):
+def fetch_crashpad():
     # If crashpad is not present, use chromium/depot_tools/fetch to clone it: note that
     # chromium/crashpad is the root directory for the build (containing metadata files
     # like `.gclient`) while chromium/crashpad/crashpad is the root of the repo
@@ -76,25 +194,27 @@ def fetch_crashpad(update=True):
         os.makedirs(__crashpad_build_root__, exist_ok=True)
         _run_depot_tool(['fetch', 'crashpad'], __crashpad_build_root__)
         assert os.path.isdir(__crashpad_repo_root__)
-    elif update:
+    else:
+        print('Syncing crashpad...')
         _run_depot_tool(['gclient', 'sync'], __crashpad_build_root__)
     print(f'Crashpad is present at: {__crashpad_repo_root__}')
 
 
-def build_crashpad(num_parallel_jobs: int):
+def build_crashpad(gn_args: GnArgs, num_parallel_jobs: int):
     # depot-tools and crashpad must be present within chromium/
     assert os.path.isdir(__depot_tools_root__)
     assert os.path.isdir(__crashpad_repo_root__)
 
     # Use gn gen to create Ninja build files
-    relpath = os.path.join('out', 'Default')
-    _run_depot_tool(['gn', 'gen', relpath], __crashpad_repo_root__)
-    _run_depot_tool(['ninja', '-C', relpath, '-j', str(num_parallel_jobs)], __crashpad_repo_root__)
+    print('Invoking GN to generate Ninja build files for crashpad...')
+    _run_depot_tool(['gn', 'gen', __crashpad_out_relpath__, f'--args={str(gn_args)}'], __crashpad_repo_root__)
+    print('Invoking Ninja to build crashpad...')
+    _run_depot_tool(['ninja', '-C', __crashpad_out_relpath__, '-j', str(num_parallel_jobs)], __crashpad_repo_root__)
 
 
 def run_crashpad_tests():
     for binary_name in __crashpad_test_binaries__:
-        binary_path = os.path.join(__crashpad_repo_root__, 'out', 'Default', binary_name)
+        binary_path = os.path.join(__crashpad_out_abspath__, binary_name)
         env = os.environ.copy()
         if sys.platform == 'win32':
             binary_path += '.exe'
@@ -106,12 +226,61 @@ def run_crashpad_tests():
     print('All crashpad tests passed.')
 
 
+def install_main(args: argparse.Namespace):
+    clone_depot_tools()
+    fetch_crashpad()
+
+
+def gn_args_main(args: argparse.Namespace):
+    _run_depot_tool(['gn', 'args', __crashpad_out_relpath__, '--list'], __crashpad_repo_root__)
+
+
+def build_main(args: argparse.Namespace):
+    # Seamlessly install tools and sync the crashpad repo unless instructed not to
+    if not args.no_install:
+        clone_depot_tools()
+        fetch_crashpad()
+
+    # Parse '-c FOO=ON -c BAR=bar' into {'FOO': 'ON', 'BAR': 'bar'}
+    cmake_vars: Dict[str, str] = {}
+    for s in args.cmake_vars:
+        if '=' not in s:
+            raise ValueError(f"Unexpected format for CMake var: '{s}'")
+        k, v = s.split('=', 1)
+        cmake_vars[k] = v
+
+    # If we've been provided with any CMake args, resolve the corresponding set of gn
+    # args to ensure that our crashpad build is binary-compatible with our CMake build
+    gn_args = GnArgs()
+    if cmake_vars:
+        gn_args = GnArgs.from_cmake(cmake_vars)
+
+    print(f'PARALLEL JOB COUNT IS: {args.parallel}')
+
+    # Use gn and ninja to produce a build of crashpad
+    build_crashpad(gn_args, args.parallel)
+
+    # Run crashpad tests to validate the build, unless instructed not to
+    if not args.no_test:
+        run_crashpad_tests()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--parallel', '-j', type=int, default=os.cpu_count() - 1, help='Number of parallel build jobs to use when invoking ninja')
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers(dest='command', required=True)
 
-    clone_depot_tools()
-    fetch_crashpad(update=True)
-    build_crashpad(num_parallel_jobs=args.parallel)
-    run_crashpad_tests()
+    install_parser = subparsers.add_parser('install')
+    install_parser.set_defaults(func=install_main)
+
+    gn_args_parser = subparsers.add_parser('gn-args')
+    gn_args_parser.set_defaults(func=gn_args_main)
+
+    build_parser = subparsers.add_parser('build')
+    build_parser.add_argument('--no-install', '-I', action='store_true', help='If set, skip install/sync of crashpad and depot_tools')
+    build_parser.add_argument('--no-test', '-T', action='store_true', help='If set, skip running crashpad tests post-build')
+    build_parser.add_argument('--cmake-var', '-c', action='append', dest='cmake_vars', default=[], help='CMake var in KEY=VALUE format; used to configure GN build')
+    build_parser.add_argument('--parallel', '-j', type=int, default=_default_parallel_job_count(), help='Number of parallel build jobs to use when invoking ninja')
+    build_parser.set_defaults(func=build_main)
+
+    args = parser.parse_args()
+    args.func(args)
