32d4b670d2947e9d698f45641fd06a611282d29c
Alex Forsythe <alex.forsythe@datadoghq.com>
2026-03-06 17:23:46 -0500

    chore: Widen margin of error for non-deterministic sampling rate tests

--- tests/c/logging_c_api_test.cpp
+++ tests/c/logging_c_api_test.cpp
@@ -839,11 +839,11 @@ TEST_CASE("dd_logger_log", "[unit][logging][c-api]") {
     }
     dd_core_stop(core);
 
-    // Then we should have sent ~250 log messages, +/- 30
+    // Then we should have sent ~250 log messages, +/- 50
     REQUIRE(test.client.requests.size() > 0);
     auto events = MergeJsonArrays(test.client.requests);
-    REQUIRE(events.size() >= 220);
-    REQUIRE(events.size() <= 280);
+    REQUIRE(events.size() >= 200);
+    REQUIRE(events.size() <= 300);
 
     // And they all should be 'info' messages
     const size_t num_info =
