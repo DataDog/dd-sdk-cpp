e5a008e280ed44338dae3a774b99db2c37762d0c
Alex Forsythe <alex.forsythe@datadoghq.com>
2026-03-13 08:43:46 -0400

    chore: Change platform to `mac` for macOS crashes

--- tools/process-crash-report/lib/formatters.py
+++ tools/process-crash-report/lib/formatters.py
@@ -314,7 +314,7 @@ def format_rum_error_event(
 
     _PLATFORM_SOURCE_TYPE = {
         "win32": "pe",
-        "darwin": "ios",
+        "darwin": "mac",
         "linux": "elf",
     }
     platform = _infer_platform(report.modules)
