a1f8464c4025c193a1ead47d22136063713ae134
Alex Forsythe <alex.forsythe@datadoghq.com>
2026-04-02 11:48:38 -0400

    fix(msvc): Define NOMINMAX before windows.h include

--- src/datadog/impl/storage/filesystem.hpp
+++ src/datadog/impl/storage/filesystem.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #ifdef _WIN32
+#define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #endif
--- tests/mock/filesystem_new.cpp
+++ tests/mock/filesystem_new.cpp
@@ -10,6 +10,7 @@
 #include <cstring>
 
 #ifdef _WIN32
+#define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #endif
