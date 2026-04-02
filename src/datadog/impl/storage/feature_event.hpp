fea4babd4e5851c6b9e5e1cd3981f5a1435a61dd
Alex Forsythe <alex.forsythe@datadoghq.com>
2026-04-02 11:43:28 -0400

    chore: Add documentation comments

--- src/datadog/impl/storage/feature_event.hpp
+++ src/datadog/impl/storage/feature_event.hpp
@@ -14,15 +14,36 @@
 
 namespace datadog::impl {
 
+/**
+ * Provides access to the event storage directories for a single feature within this SDK
+ * instance and process, at <application-storage>/.datadog/<instance>/<pid>/<feature>/.
+ *
+ * Event data is segregated into two subdirectories based on tracking consent state: a
+ * pending directory for events buffered while consent is unknown, and a granted
+ * directory for events that may be uploaded.
+ *
+ * FeatureEventStorage is responsible for creating the requisite directories and for
+ * handling deletion and/or migration of the files within them. For the actual logic
+ * used to persist events to disk at runtime, see `BatchWriter`.
+ */
 class FeatureEventStorage {
  public:
   explicit FeatureEventStorage(IFilesystem& in_fs, impl::DiagnosticLogger& in_logger);
 
+  /**
+   * Given the path to <application-storage>/.datadog/<instance>/<pid>/, creates a
+   * subdirectory for the given feature (if none yet exists), and populates it with the
+   * requisite consent-level directories where batch files may be stored.
+   *
+   * Returns true if all required directories now exist.
+   */
   bool Initialize(std::string_view process_root, std::string_view feature_name);
 
   /**
    * Returns the path to the directory where batches of event data should be stored for
    * this feature, within this SDK instance, while tracking consent is pending.
+   *
+   * May only be called after Initialize() has completed successfully.
    */
   std::string_view GetPendingPath() const;
 
@@ -30,6 +51,8 @@ class FeatureEventStorage {
    * Returns the path to the directory where batches of event data are stored for this
    * feature once the SDK instance has been granted tracking consent. This is the only
    * directory that events may be uploaded from.
+   *
+   * May only be called after Initialize() has completed successfully.
    */
   std::string_view GetGrantedPath() const;
 
