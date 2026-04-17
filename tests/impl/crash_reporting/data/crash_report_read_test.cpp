fbf5555e230d905be25d9af44bcb2414c7c5ebd6
Alex Forsythe <alex.forsythe@datadoghq.com>
2026-04-17 12:01:04 -0400

    fix: Add unit tests for module and stack frame limits

--- tests/impl/crash_reporting/data/crash_report_read_test.cpp
+++ tests/impl/crash_reporting/data/crash_report_read_test.cpp
@@ -233,6 +233,158 @@ TEST_CASE("ReadCrashReport", "[unit][crash_reporting]") {
     REQUIRE(result.empty == false);
   }
 
+  SECTION("M return Malformed W file contains more than 4096 loaded modules") {
+    // Given two scenarios: one where our file will contain exactly the maximum number
+    // of modules, and another where it exceeds the limit by 1
+    auto target_module_count = GENERATE(as<size_t>(), 4096, 4097);
+
+    // Given a file that contains valid crash report data, but an excessive number of
+    // loaded module entries
+    {
+      // Find the offsets of the first and second module entry in our example file
+      std::string_view magic{reinterpret_cast<const char*>(&CrashReportModuleMagic), 8};
+      const auto first_module_pos = data.find(magic);
+      REQUIRE(first_module_pos != std::string_view::npos);
+      const auto second_module_pos = data.find(magic, first_module_pos + 8);
+      REQUIRE(second_module_pos != std::string_view::npos);
+
+      // Count any remaining modules in the original file, so we can populate the file
+      // with exactly our desired number of modules
+      size_t num_modules_in_file = 2;
+      for (auto pos = second_module_pos + 8;
+           (pos = data.find(magic, pos)) != std::string_view::npos;
+           pos += 8) {
+        ++num_modules_in_file;
+      }
+      REQUIRE(num_modules_in_file < target_module_count);
+
+      // Extract the span of bytes representing the data for the first module entry
+      const size_t first_module_size = second_module_pos - first_module_pos;
+      std::string_view first_module{data.data() + first_module_pos, first_module_size};
+
+      // Build a string that repeats that module N times
+      const size_t num_extra_modules = target_module_count - num_modules_in_file;
+      std::string extra_module_data;
+      extra_module_data.reserve(first_module.size() * num_extra_modules);
+      for (size_t i = 0; i < num_extra_modules; i++) {
+        extra_module_data += first_module;
+      }
+
+      // Prepend that extra data to to the start of the file's module list, so that it
+      // now has `num_extra_modules` additional modules, in addition to the entries that
+      // already existed in the file
+      std::string_view head{data.data(), first_module_pos};
+      std::string_view tail{
+          data.data() + first_module_pos, data.size() - first_module_pos
+      };
+      std::string new_data = std::string(head) + extra_module_data + std::string(tail);
+      fs.Touch("crash", new_data);
+    }
+
+    // And an open handle to that file
+    const bool hold_advisory_lock = false;
+    auto open_res = fs.Wrapper().OpenForRead("crash", hold_advisory_lock);
+    REQUIRE(open_res.value == FilesystemResult::OK);
+
+    // When we parse that file as a crash report
+    auto result = ReadCrashReport(open_res.file);
+
+    // Then the result depends on whether we've exceeded the limit:
+    if (target_module_count == 4096) {
+      // At the limit: no issue; we get valid data
+      REQUIRE(result.GetStatus() == ReadCrashReportResult::Status::OK);
+      REQUIRE(result.data.has_value());
+      REQUIRE(result.data->modules.size() == 4096);
+      REQUIRE(result.fs_result == FilesystemResult::OK);
+      REQUIRE(result.empty == false);
+    } else {
+      // Over the limit: file is treated as malformed - the function aborts rather than
+      // allowing our result value's modules vector to grow unbounded
+      REQUIRE(target_module_count == 4097);
+      REQUIRE(result.GetStatus() == ReadCrashReportResult::Status::Malformed);
+      REQUIRE(!result.data.has_value());
+      REQUIRE(result.fs_result == FilesystemResult::OK);
+      REQUIRE(result.empty == false);
+    }
+  }
+
+  SECTION("M return Malformed W file contains more than 512 stack frames") {
+    // Given two scenarios: one where our file will contain exactly the maximum number
+    // of stack frames, and another where it exceeds the limit by 1
+    auto target_frame_count = GENERATE(as<size_t>(), 512, 513);
+
+    // Given a file that contains valid crash report data, but an excessive number of
+    // stack frame entries
+    {
+      // Find the offsets of the first and second stack frame entry in our example file
+      std::string_view magic{
+          reinterpret_cast<const char*>(&CrashReportStackFrameMagic), 8
+      };
+      const auto first_frame_pos = data.find(magic);
+      REQUIRE(first_frame_pos != std::string_view::npos);
+      const auto second_frame_pos = data.find(magic, first_frame_pos + 8);
+      REQUIRE(second_frame_pos != std::string_view::npos);
+
+      // Count any remaining stack frames in the original file, so we can populate the
+      // file with exactly our desired number of frames
+      size_t num_frames_in_file = 2;
+      for (auto pos = second_frame_pos + 8;
+           (pos = data.find(magic, pos)) != std::string_view::npos;
+           pos += 8) {
+        ++num_frames_in_file;
+      }
+      REQUIRE(num_frames_in_file < target_frame_count);
+
+      // Extract the span of bytes representing the data for the first stack frame entry
+      const size_t first_frame_size = second_frame_pos - first_frame_pos;
+      std::string_view first_frame{data.data() + first_frame_pos, first_frame_size};
+
+      // Build a string that repeats that frame N times
+      const size_t num_extra_frames = target_frame_count - num_frames_in_file;
+      std::string extra_frame_data;
+      extra_frame_data.reserve(first_frame.size() * num_extra_frames);
+      for (size_t i = 0; i < num_extra_frames; i++) {
+        extra_frame_data += first_frame;
+      }
+
+      // Prepend that extra data to the start of the file's stack frame list, so that
+      // it now has `num_extra_frames` additional frames, in addition to the entries
+      // that already existed in the file
+      std::string_view head{data.data(), first_frame_pos};
+      std::string_view tail{
+          data.data() + first_frame_pos, data.size() - first_frame_pos
+      };
+      std::string new_data = std::string(head) + extra_frame_data + std::string(tail);
+      fs.Touch("crash", new_data);
+    }
+
+    // And an open handle to that file
+    const bool hold_advisory_lock = false;
+    auto open_res = fs.Wrapper().OpenForRead("crash", hold_advisory_lock);
+    REQUIRE(open_res.value == FilesystemResult::OK);
+
+    // When we parse that file as a crash report
+    auto result = ReadCrashReport(open_res.file);
+
+    // Then the result depends on whether we've exceeded the limit:
+    if (target_frame_count == 512) {
+      // At the limit: no issue; we get valid data
+      REQUIRE(result.GetStatus() == ReadCrashReportResult::Status::OK);
+      REQUIRE(result.data.has_value());
+      REQUIRE(result.data->stack_addresses.size() == 512);
+      REQUIRE(result.fs_result == FilesystemResult::OK);
+      REQUIRE(result.empty == false);
+    } else {
+      // Over the limit: file is treated as malformed - the function aborts rather than
+      // allowing our result value's stack_addresses vector to grow unbounded
+      REQUIRE(target_frame_count == 513);
+      REQUIRE(result.GetStatus() == ReadCrashReportResult::Status::Malformed);
+      REQUIRE(!result.data.has_value());
+      REQUIRE(result.fs_result == FilesystemResult::OK);
+      REQUIRE(result.empty == false);
+    }
+  }
+
   SECTION("M return ReadError W file can not be read due to filesystem error") {
     // Given a file that contains our valid crash report data, but that the filesystem
     // will prevent us from reading
